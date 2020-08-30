af7415a76463f65b43341e1221cd41aa6f0be9a9
abhishekp6 <53012981+abhishekp6@users.noreply.github.com>
2020-08-31 02:05:51 +0530

    Create Ticket model in api database

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Ticket(models.Model):
+    ticket_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    contact_number = models.IntegerField()
+    start_time = models.DateTimeField(auto_now=False, auto_now_add=False) # Time when movie show will start
+    current_time = models.DateTimeField(auto_now=True, auto_now_add=False) # Time When ticket is booked
+    status = models.CharField(max_length=10, default='Active')
+
+    def __str__(self):
+        return self.name
